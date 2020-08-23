df173818b5ac6be24501af35e6130e29aeb059ed
Gihun Ham <ham@e-mirim.hs.kr>
2020-08-23 14:37:42 +0900

    #3 타이머 영역 UI 작성

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -48,7 +49,54 @@ class MyApp extends StatelessWidget {
    }
 
   Widget _buildBody() {
-     return Container();
+     return Center(
+       child: Padding(
+         padding: const EdgeInsets.only(top: 30),
+         child: Stack(
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Row( // 시간을 표시하는 영역
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    Text( // 초
+                       '0',
+                       style: TextStyle(fontSize: 50.0),
+                       ),
+                    Text('00'), // 1/100초
+                  ],
+                ),
+                Container(  // 랩타임을 표시하는 영역
+                  width: 100,
+                  height: 200,
+                  child: ListView(
+                    children: <Widget>[], // TODO : 랩타임 추후 수정
+                  ),
+                )
+              ],
+            ),
+            Positioned( // TODO: 왼쪽 아래 위치 초기화 버튼
+              left:10,
+              bottom: 10,
+              child: FloatingActionButton(
+                backgroundColor: Colors.deepOrangeAccent,
+                onPressed: () {}, // TODO: 왼쪽 아래 위치 초기화 버튼 동작 구현
+                child: Icon(Icons.rotate_left),
+              ),
+            ),
+            Positioned( // 오른쪽 아래에 위치한 랩타임 버튼
+              right:10,
+              bottom: 10,
+              child: RaisedButton(
+                onPressed: () {}, // TODO: 오른쪽 아래에 위치한 랩타임 버튼 동작 구현
+                child: Text('랩타임'),
+              )
+            ),
+          ],
+         )
+       )
+     );
   }
 
   void _clickButton() {}
